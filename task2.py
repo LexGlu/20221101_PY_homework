fda1210e22f923609fd3ebb21911e0aba1a6efbf
LexGlu <117204585+LexGlu@users.noreply.github.com>
2022-11-02 19:23:48 +0200

    Update task2.py
    added comments

--- task2.py
+++ task2.py
@@ -1,7 +1,14 @@
 def test_print():
+    # Alexa Siri Bixby
     print('Alexa', 'Siri', 'Bixby')
+
+    # Alexa ==> Siri ==> Bixby
     print('Alexa', 'Siri', 'Bixby', sep=' ==> ')
+
+    # Alexa Siri Bixby!
     print('Alexa', 'Siri', 'Bixby', end='!\n')
+
+    # Alexa and Siri and Bixby are virtual assistants.
     print('Alexa', 'Siri', 'Bixby', sep=' and ', end=' are virtual assistants.')
 
 
