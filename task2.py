0c4ca10647a21d55d1a0f0c31cea3ccf3f942899
LexGlu <117204585+LexGlu@users.noreply.github.com>
2022-11-02 19:10:41 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,8 @@
+def test_print():
+    print('Alexa', 'Siri', 'Bixby')
+    print('Alexa', 'Siri', 'Bixby', sep=' ==> ')
+    print('Alexa', 'Siri', 'Bixby', end='!\n')
+    print('Alexa', 'Siri', 'Bixby', sep=' and ', end=' are virtual assistants.')
+
+
+test_print()
---
+++ task3.py
@@ -0,0 +1,8 @@
+def oh():
+    line1 = '#' * 9
+    line2 = '#\t\t#'
+    print(line1, line2, line2, line2, line1, sep='\n')
+    print('\n', line2, line2, line1, line2, line2, sep='\n')
+
+
+oh()
