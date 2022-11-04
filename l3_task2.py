1075082b60cdcb707131b27ec9823d3c04a7fc2d
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2022-11-04 22:12:50 +0200

    Add files via upload
    3. Змінні та примітивні типи даних. Task 2 and Task 3

---
+++ l3_task2.py
@@ -0,0 +1,8 @@
+def concat():
+    first_name = 'Oleksandr'
+    last_name = 'Glushchenko'
+    greeting = 'Hello ' + first_name + ' ' + last_name + '!'
+    print(greeting)
+
+
+concat()
---
+++ l3_task3.py
@@ -0,0 +1,13 @@
+def calculator():
+    a = 11
+    b = 7
+    print(f'{a} + {b} = {a + b}')
+    print(f'{a} - {b} = {a - b}')
+    print(f'{a} / {b} = {a / b}')
+    print(f'{a} * {b} = {a * b}')
+    print(f'{a} ** {b} = {a ** b}')
+    print(f'{a} % {b} = {a % b}')
+    print(f'{a} // {b} = {a // b}')
+
+
+calculator()
