245950a7e068d8c1bfca29a429e2e7f2b87d4d16
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2022-11-05 20:07:38 +0200

    Update l4_task3.py

--- l4_task3.py
+++ l4_task3.py
@@ -6,8 +6,9 @@ def math_quiz():
     a = random.randint(1, 10)
     b = random.randint(1, 10)
     operator = random.choice(operators)
-    res = eval(f'{a} {operator} {b}')
-    answer = input(f'Enter answer for the following mathematical expression: {a} {operator} {b}\n')
+    quiz = f'{a} {operator} {b}'
+    res = eval(quiz)
+    answer = input(f'Enter answer for the following mathematical expression: {quiz}\n')
     if answer.isdigit() or answer[0] == '-' and answer[1:].isdigit():
         if int(answer) == res:
             print("That's correct!")
@@ -19,7 +20,5 @@ def math_quiz():
     if next_try == 'y':
         math_quiz()
 
-
-
-
+        
 math_quiz()
